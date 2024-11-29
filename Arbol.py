d2d5e3d58127c6cfcb130ee0066a89a23eeafcd4
rubences <91731294+rubences@users.noreply.github.com>
2024-11-29 16:14:59 +0000

    mas cosas

---
+++ Arbol.py
@@ -0,0 +1,45 @@
+from NodoArbol import Nodo
+
+
+class Arbol:
+    def __init__(self):
+        self.raiz = None
+
+    def insertar(self, valor):
+        if self.raiz is None:
+            self.raiz = Nodo(valor)
+        else:
+            self._insertar_recursivo(self.raiz, valor)
+
+    def _insertar_recursivo(self, nodo, valor):
+        if valor < nodo.valor:
+            if nodo.izquierda is None:
+                nodo.izquierda = Nodo(valor)
+            else:
+                self._insertar_recursivo(nodo.izquierda, valor)
+        else:
+            if nodo.derecha is None:
+                nodo.derecha = Nodo(valor)
+            else:
+                self._insertar_recursivo(nodo.derecha, valor)
+
+    def buscar(self, valor):
+        return self._buscar_recursivo(self.raiz, valor)
+
+    def _buscar_recursivo(self, nodo, valor):
+        if nodo is None or nodo.valor == valor:
+            return nodo
+        if valor < nodo.valor:
+            return self._buscar_recursivo(nodo.izquierda, valor)
+        return self._buscar_recursivo(nodo.derecha, valor)
+
+    def en_orden(self):
+        elementos = []
+        self._en_orden_recursivo(self.raiz, elementos)
+        return elementos
+
+    def _en_orden_recursivo(self, nodo, elementos):
+        if nodo:
+            self._en_orden_recursivo(nodo.izquierda, elementos)
+            elementos.append(nodo.valor)
+            self._en_orden_recursivo(nodo.derecha, elementos)
---
+++ NodoArbol.py
@@ -0,0 +1,5 @@
+class Nodo:
+    def __init__(self, valor):
+        self.valor = valor
+        self.izquierda = None
+        self.derecha = None
